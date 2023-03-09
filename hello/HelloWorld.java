dc2d24ed8d193cd307132fb0bf5b256c9159fbf0
COHERWEB <tihor@COHERWEBs-MacBook-Pro.local>
2023-03-09 10:15:29 +0100

    commit 1

---
+++ hello/HelloWorld.java
@@ -0,0 +1,14 @@
+package hello;
+
+import sayhello.HeyYou;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        HeyYou h = new HeyYou();
+
+        System.out.println("Helloworld, i'm from HelloWorld, how are You?");
+        System.out.println(h);
+
+    }
+}
---
+++ sayhello/HeyYou.java
@@ -0,0 +1,7 @@
+package sayhello;
+
+public class HeyYou {
+    public HeyYou() {
+        System.out.println("Hello , i'm from HeyYou and i'm great, how are You?");
+    }
+}
