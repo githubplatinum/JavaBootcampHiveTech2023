afced9a65d6a955167b1c24380b6896a0ebf4b60
Tihomir <trecic@gmail.com>
2023-03-10 15:58:23 +0100

    commit 1

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.File;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        File file = new File( "konverzijskiFaktor.txt");
+        FileWriter writer = new FileWriter(file);
+
+        System.out.println("Unesitne broj kilometara: ");
+        double brKilometara = scanner.nextDouble();
+
+
+        System.out.println("Unesite konverzijski faktor: ");
+        double konverzijskiFaktor = scanner.nextDouble();
+
+        writer.write(Double.toString(konverzijskiFaktor));
+        writer.close();
+
+        FileReader reader = new FileReader(file);
+        char[] buffer = new char[30];
+        reader.read(buffer);
+        reader.close();
+        String contents = new String(buffer);
+        System.out.println(contents);
+
+        System.out.println(contents.equals(konverzijskiFaktor));
+
+        if (brKilometara < 0)
+            System.out.println("Broj kilometara mora biti pozitivan broj");
+        else if (konverzijskiFaktor < 0) {
+
+            System.out.println("Konverzijski faktor mora biti pozitivan broj");
+        } else {
+            int rezultatKonverzije = (int) (brKilometara / konverzijskiFaktor);
+            System.out.println("Uneseno je "+ brKilometara + " kilometara, što je " + rezultatKonverzije + " milja");
+        }
+    }
+
+
+
+}
+
+
+
